ef7345f3bda34b2b45522287cd0d0765a00f5a5c
Devi Aditya Varma Dantuluri <ddavarma@gmail.com>
2025-02-26 16:44:51 -0800

    Fancy counter app

---
+++ src/Count.jsx
@@ -0,0 +1,4 @@
+export default function Count({ count }) {
+  console.log("Componenet rendering");
+  return <p className="count">{count}</p>;
+}
---
+++ src/Title.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+export default function Title({ locked }) {
+  return (
+    <h1 className="title">
+      {locked ? (
+        <span>
+          Limit! Buy <b>Pro</b> for &gt;5{" "}
+        </span>
+      ) : (
+        "Fancy Counter"
+      )}
+    </h1>
+  );
+}
